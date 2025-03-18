1b7ae3bbc02cb12e7b0f2b83884b2102ae57378a
RicardoAyala2003 <92557271+RicardoAyala2003@users.noreply.github.com>
2025-03-18 13:06:24 -0600

    avance

--- proyecto/main.py
+++ proyecto/main.py
@@ -1,39 +1,28 @@
 from MnistDataset import MnistDataset
-from red import NeuralNetwork  
+from red import NeuralNetwork
 import numpy as np
 
-def main():
-    print("Cargando datos de entrenamiento...")
-    
-   
-    mnist_train = MnistDataset()
-    mnist_train.load("/home/ricardo/Redes/Mnist/dataset/train-images-idx3-ubyte", 
-                     "/home/ricardo/Redes/Mnist/dataset/train-labels-idx1-ubyte")
-    
-    mnist_test = MnistDataset()
-    mnist_test.load("/home/ricardo/Redes/Mnist/dataset/t10k-images-idx3-ubyte", 
-                    "/home/ricardo/Redes/Mnist/dataset/t10k-labels-idx1-ubyte")
+mnist_train = MnistDataset()
+mnist_train.load("dataset/train-images-idx3-ubyte", "dataset/train-labels-idx1-ubyte")
+mnist_test = MnistDataset()
+mnist_test.load("dataset/t10k-images-idx3-ubyte", "dataset/t10k-labels-idx1-ubyte")
 
-    X_train = mnist_train.get_flattened_data()
-    y_train = mnist_train.get_one_hot_labels()
-    X_test = mnist_test.get_flattened_data()
-    y_test = mnist_test.get_one_hot_labels()
 
-    
-    input_size = 784  
-    hidden_size = 128  
-    output_size = 10  
-    learning_rate = 0.1  
+X_train = mnist_train.get_flattened_data()
+y_train = mnist_train.get_one_hot_labels()
+X_test = mnist_test.get_flattened_data()
+y_test = mnist_test.get_one_hot_labels()
 
-    
-    nn = NeuralNetwork(input_size, hidden_size, output_size, learning_rate)
 
-    nn.train(X_train, y_train, epochs=10)
+input_size = 784 
+hidden_size = 128
+output_size = 10
+learning_rate = 0.1
+nn = NeuralNetwork(input_size, hidden_size, output_size, learning_rate)
 
-    y_test_pred = nn.predict(X_test)
-    accuracy = np.mean(np.argmax(y_test, axis=1) == y_test_pred)
-    
-    print(f"Accuracy: [{accuracy * 100:.2f}%]")
+nn.train(X_train, y_train, epochs=10)
 
-if __name__ == "__main__":
-    main()
+
+y_test_pred = nn.predict(X_test)
+accuracy = np.mean(np.argmax(y_test, axis=1) == y_test_pred)
+print("Accuracy: [" +  str(accuracy * 100) + "%]")
--- proyecto/red.py
+++ proyecto/red.py
@@ -1,9 +1,8 @@
 import numpy as np
 from DenseLayer import DenseLayer, ReLU, Softmax, CrossEntropy
 from MnistDataset import MnistDataset
-
 class NeuralNetwork:
-    def __init__(self, input_size, hidden_size, output_size, learning_rate=0.1):
+    def __init__(self, input_size, hidden_size, output_size, learning_rate=0.1):  # Reducir la tasa de aprendizaje
         self.capa1 = DenseLayer(input_size, hidden_size)
         self.activation1 = ReLU()
         self.capa2 = DenseLayer(hidden_size, output_size)
@@ -11,8 +10,8 @@ class NeuralNetwork:
         self.loss_function = CrossEntropy()
         self.learning_rate = learning_rate
 
-        self.capa1.cargar_pesos("Mnist/pesosguardados")
-        self.capa2.cargar_pesos("Mnist/pesosguardados")
+        self.capa1.weights_loader("Mnist/pesosguardados")
+        self.capa2.weights_loader("Mnist/pesosguardados")
 
     def forward(self, X):
         self.z1 = self.capa1.forward(X)
@@ -22,21 +21,21 @@ class NeuralNetwork:
         return self.a2
 
     def backward(self, X, y_true, y_pred):
-        grad_loss = self.loss_function.backward(y_pred, y_true)
-        grad_a2 = self.activation2.backward(grad_loss)
-        grad_z2 = self.capa2.backward(grad_a2)
+        grad_loss = self.loss_function.compute_gradient(y_true, y_pred)
+        grad_a2 = self.activation2.backward(grad_loss, y_pred)
+        grad_z2 = self.capa2.backward(grad_a2, self.learning_rate)
         grad_a1 = self.activation1.backward(grad_z2)
-        self.capa1.backward(grad_a1)
+        self.capa1.backward(grad_a1, self.learning_rate)
 
     def train(self, X, y, epochs):
         for epoch in range(epochs):
             y_pred = self.forward(X)
-            loss = self.loss_function.forward(y_pred, y)
+            loss = self.loss_function.compute_loss(y, y_pred)
             self.backward(X, y, y_pred)
-
+           
             print(f"Epoch [{epoch}] ---- Loss: [{loss:.4f}]")
-            self.capa1.guardar_pesos("Mnist/pesosguardados")
-            self.capa2.guardar_pesos("Mnist/pesosguardados")
+            self.capa1.weights_saver("Mnist/pesosguardados")
+            self.capa2.weights_saver("Mnist/pesosguardados")
 
     def predict(self, X):
         return np.argmax(self.forward(X), axis=1)
